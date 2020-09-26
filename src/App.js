d1131b55ca5b0a24dd9e3635bf5a1f19acb2a58e
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2020-09-26 11:06:58 -0500

    added routes and render prop

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NameForm from './NameForm';
 import GrandChildNeedsContext from './GrandChildNeedsContext';
 import NewNameForm from './NewNameForm';
 import NameContext from './NameContext';
+import { BrowserRouter, Route, Link } from "react-router-dom";
 
 class App extends Component {
   constructor(props) {
@@ -20,20 +21,17 @@ class App extends Component {
       lName: lname
     })
   }
-
   addNewName(fname, lname) {
     this.setState({
       fnanme2: fname,
       lname2: lname
     })
   }
-
   handleFormSubmit = (event) => {
     event.preventDefault();
     const { firstName, lastName } = event.target;
     this.updateName(firstName.value, lastName.value);
   }
-
   handleNewFormSubmit = (event) => {
     event.preventDefault();
     console.log('handleNewFormSubmit ran', event.target);
@@ -56,6 +54,25 @@ class App extends Component {
         <NameContext.Provider value={contextValue}>
           <GrandChildNeedsContext />
         </NameContext.Provider>
+
+        <Link to='/new' >Go to new form</Link>
+        <br />
+        <Link to='/' >Go Home</Link>
+
+        {/* normal */}
+        <Route path='/new' component={NewNameForm} />
+        {/* Render prop */}
+        <Route
+          exact path="/props-through-render"
+          render={(props) =>
+            <PropsPage {...props}
+              title={`Props through render`}
+              onAddBookmark={this.addBookmark}
+            />
+          }
+        />
+
+
         <NewNameForm handleSubmit={this.handleNewFormSubmit} />
       </Fragment>
     );
